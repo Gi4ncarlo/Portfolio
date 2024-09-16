1fb8fd74cbbaef26b68f4a1273c0e90413de8b20
Giancarlo <giancarlozanarini9@gmail.com>
2024-09-16 12:25:53 -0300

    Primer carga portfolio

---
+++ Portfolio/astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+
+import tailwind from "@astrojs/tailwind";
+
+// If you are not going to use vercel analytics you can delete the analytics
+// If you want to remove vercel from your project you can do npm uninstall vercel
+export default defineConfig({
+  output: "server",
+
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+
+  integrations: [tailwind()],
+});
+
+//If you don't have vercel the configuration will be empty
+
+//ej : export default defineConfig({});
