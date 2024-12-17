9258459ba5571257bab9b82390bceda1d3a5f45e
Giancarlo <giancarlozanarini9@gmail.com>
2024-12-17 11:52:13 -0300

    Deploy v1.5 Portfolio

--- Portfolio/astro.config.mjs
+++ Portfolio/astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercelStatic from "@astrojs/vercel/static";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
+  output: "static",
+  adapter: vercelStatic(),
   integrations: [tailwind(), react()],
 });
