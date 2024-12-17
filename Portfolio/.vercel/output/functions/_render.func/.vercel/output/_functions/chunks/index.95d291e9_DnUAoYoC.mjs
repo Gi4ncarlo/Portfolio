e4640d991c47067c02c09627b638c2009a5bb5a3
Giancarlo <giancarlozanarini9@gmail.com>
2024-12-17 11:39:27 -0300

    Deploy v1.3 Portfolio

---
+++ Portfolio/.vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index.95d291e9_DnUAoYoC.mjs
@@ -0,0 +1,8 @@
+const scrollToTop = "_scrollToTop_9ft56_1";
+const button = "_button_9ft56_15";
+const styles = {
+	scrollToTop: scrollToTop,
+	button: button
+};
+
+export { styles as s };
---
+++ Portfolio/.vercel/output/static/_astro/index.95d291e9.sTGqqvEv.js
@@ -0,0 +1,1 @@
+const o="_scrollToTop_9ft56_1",t="_button_9ft56_15",s={scrollToTop:o,button:t};export{s};
