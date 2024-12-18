5d81be8fc975199cbb6b9846e01c011f7bd2d96f
Giancarlo <giancarlozanarini9@gmail.com>
2024-12-18 10:32:03 -0300

    Deploy v2.3 Portfolio

--- Portfolio/src/components/scrollToUpBtn.jsx
+++ Portfolio/src/components/scrollToUpBtn.jsx
@@ -1,43 +1,43 @@
-import { useState, useEffect } from 'react';
-import styles from '../style/ScrollToUpBtn.module.css';
+// import { useState, useEffect } from 'react';
+// import styles from '../style/ScrollToUpBtn.module.css';
 
-const ScrollToTopButton = () => {
-  const [visible, setVisible] = useState(false);
+// const ScrollToTopButton = () => {
+//   const [visible, setVisible] = useState(false);
 
-  const toggleVisibility = () => {
-    if (window.pageYOffset > 300) {
-      setVisible(true);
-      console.log("Que hace pibe scroleando");
+//   const toggleVisibility = () => {
+//     if (window.pageYOffset > 300) {
+//       setVisible(true);
+//       console.log("Que hace pibe scroleando");
       
 
-    } else {
-      setVisible(false);
-    }
-  };
+//     } else {
+//       setVisible(false);
+//     }
+//   };
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
-  };
+//   const scrollToTop = () => {
+//     window.scrollTo({
+//       top: 0,
+//       behavior: 'smooth',
+//     });
+//   };
 
-  useEffect(() => {
-    window.addEventListener('scroll', toggleVisibility);
-    return () => {
-      window.removeEventListener('scroll', toggleVisibility);
-    };
-  }, []);
+//   useEffect(() => {
+//     window.addEventListener('scroll', toggleVisibility);
+//     return () => {
+//       window.removeEventListener('scroll', toggleVisibility);
+//     };
+//   }, []);
 
-  return (
-    <div className={styles.scrollToTop}>
-      {visible && (
-        <button onClick={scrollToTop} className={styles.button}>
-          ↑
-        </button>
-      )}
-    </div>
-  );
-};
+//   return (
+//     <div className={styles.scrollToTop}>
+//       {visible && (
+//         <button onClick={scrollToTop} className={styles.button}>
+//           ↑
+//         </button>
+//       )}
+//     </div>
+//   );
+// };
 
-export default ScrollToTopButton;
+// export default ScrollToTopButton;
